4b329831b02fa821c97f995319c7ac57ba2be49b
Sagi Yemini <sagi@for-each.com>
2016-12-25 12:04:00 +0200

    Fix library and sample config

--- sample/src/main/java/com/autodesk/customspinner/sample/MainActivity.java
+++ sample/src/main/java/com/autodesk/customspinner/sample/MainActivity.java
@@ -3,8 +3,6 @@ package com.autodesk.customspinner.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.autodesk.customspinner.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
