c343313986df816b57ddcbd6a89c0a9ed3f5ec91
Sagi Yemini <sagi@for-each.com>
2016-12-25 12:52:46 +0200

    Display first item on setAdapter

--- library/src/main/java/com/autodesk/customspinner/CustomSpinner.java
+++ library/src/main/java/com/autodesk/customspinner/CustomSpinner.java
@@ -2,7 +2,9 @@ package com.autodesk.customspinner;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.Adapter;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by sagiyemini on 25/12/2016.
@@ -10,6 +12,10 @@ import android.widget.LinearLayout;
 
 public class CustomSpinner extends LinearLayout {
 
+    private Adapter mAdapter;
+
+    private int mSelectedItemPosition;
+    private TextView mTitle;
 
     public CustomSpinner(Context context) {
         super(context);
@@ -28,6 +34,16 @@ public class CustomSpinner extends LinearLayout {
 
     private void init() {
         inflate(getContext(), R.layout.custom_spinner, this);
+        this.mTitle = (TextView) findViewById(R.id.title);
     }
 
+    public void setAdapter(Adapter adapter) {
+        this.mAdapter = adapter;
+        this.mSelectedItemPosition = 0;
+        if (adapter != null) {
+            if (adapter.getCount() > 0) {
+                mTitle.setText(adapter.getItem(mSelectedItemPosition).toString());
+            }
+        }
+    }
 }
--- sample/src/main/java/com/autodesk/customspinner/sample/MainActivity.java
+++ sample/src/main/java/com/autodesk/customspinner/sample/MainActivity.java
@@ -1,7 +1,12 @@
 package com.autodesk.customspinner.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+
+import com.autodesk.customspinner.CustomSpinner;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CustomSpinner spinner = (CustomSpinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item);
+        adapter.addAll(Arrays.asList("First", "Second", "Third", "Forth", "Fifth", "Some longer title"));
+        spinner.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
